2ee3f8c6b49b8fd9e479117abc2f600e25a7a961
slem7451 <slem7451@gmail.com>
2023-08-12 17:51:27 +0500

    create project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=school',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
